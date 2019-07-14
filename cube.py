8304c7375a64861dbc249660833fbc3179c28133
Fargekritt <Lullinj98@gmail.com>
2019-07-14 22:53:40 +0200

    Added cube.py

---
+++ cube.py
@@ -0,0 +1,9 @@
+class Cube():
+    def __init__(self, start, dirnx=0, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
