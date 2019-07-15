ae7855a1f56df0dc8c59fb617d60c2e3d56d87cb
Fargekritt <lullinj98@gmail.com>
2019-07-15 13:35:29 +0200

    Completed add_cube func

--- snake.py
+++ snake.py
@@ -69,6 +69,10 @@ class Snake(object):
             self.body.append(cube.Cube((tail.pos[0]-1, tail.pos[1])))
         elif dx == -1 and dy == 0:
             self.body.append(cube.Cube((tail.pos[0]+1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube.Cube((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube.Cube((tail.pos[0], tail.pos[1]+1)))
 
         self.body[-1].dirnx = dx
         self.body[-1].dirny = dy
