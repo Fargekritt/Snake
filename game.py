249f776d4f29ecada46d3530c5d760a91fd8ee8a
Fargekritt <lullinj98@gmail.com>
2019-07-15 10:02:49 +0200

    Change main game loop to compensate snake.move changes

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import pygame
 import snake
 
 width = 500
-height = 500
+height = width
 rows = 20
 
 
@@ -22,6 +22,7 @@ def draw_grid(w, rows, surface):
 
 def redraw_window(surface):
     surface.fill((0, 0, 0))
+    s.draw(surface)
     draw_grid(width, rows, surface)
     pygame.display.update()
 
@@ -31,30 +32,26 @@ def random_snack(rows, item):
 
 
 def main():
+    global s
     pygame.init()
     win = pygame.display.set_mode((width, height))
     pygame.display.set_caption("snake")
 
     s = snake.Snake((255, 0, 0), (10, 10))
-
     run = True
     while run:
+        keys = pygame.key.get_pressed()
         pygame.time.delay(100)
         redraw_window(win)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-            keys = pygame.key.get_pressed()
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    s.move("left")
-                elif keys[pygame.K_RIGHT]:
-                    s.move("right")
-                elif keys[pygame.K_UP]:
-                    s.move("up")
-                elif keys[pygame.K_DOWN]:
-                    s.move("down")
+        for key in keys:
+            if keys[pygame.K_a]:
+                s.add_cube()
+                break
+        s.move()
+        redraw_window(win)
     pygame.quit()
 
 
