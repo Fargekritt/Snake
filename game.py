11ad6e2ded2bdcc217d744a548dcee98f261b828
Fargekritt <lullinj98@gmail.com>
2019-07-15 13:38:29 +0200

    added random_snack

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
+import random
+
 import pygame
 
 import snake
+import cube
 
 width = 500
 height = width
@@ -24,21 +27,32 @@ def redraw_window(surface):
     surface.fill((0, 0, 0))
     s.draw(surface)
     draw_grid(width, rows, surface)
+    snack.draw(surface)
     pygame.display.update()
 
 
 def random_snack(rows, item):
-    pass
+
+    positions = item.body
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+    return (x, y)
 
 
 def main():
-    global s
+    global s, snack
     pygame.init()
     win = pygame.display.set_mode((width, height))
     pygame.display.set_caption("snake")
 
     s = snake.Snake((255, 0, 0), (10, 10))
     run = True
+    snack = cube.Cube(random_snack(rows, s))
     while run:
         keys = pygame.key.get_pressed()
         pygame.time.delay(100)
